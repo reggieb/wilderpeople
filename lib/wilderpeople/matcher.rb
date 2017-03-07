cfb3fca01692c1eaa66c9c192d232f5eac0d0b36
Rob Nichols <git@nicholshayes.co.uk>
2017-03-07 09:59:36 +0000

    Add first_letter and exact_no_space matchers, and make case insensitive

--- lib/wilderpeople/matcher.rb
+++ lib/wilderpeople/matcher.rb
@@ -2,6 +2,7 @@ module Wilderpeople
   class Matcher
 
     def exact(a,b)
+      prep(a,b)
       a == b
     end
 
@@ -14,6 +15,29 @@ module Wilderpeople
       return true if exact(a,b)
       words = [a,b].collect(&:split)
       exact *words.collect{|w| w.size == 1 ? w : w[0..-2]}
+    end
+
+    def first_letter(a,b)
+      return true if exact(a,b)
+      exact *[a,b].collect{|x| x[0]}
+    end
+
+    def exact_no_space(a,b)
+      return true if exact(a,b)
+      exact *[a,b].collect{|x| x.gsub(/\s/, '')}
+    end
+
+    def prep(*args)
+      args.each do |a|
+        case a
+        when String
+          a.downcase!
+        when Array
+          a.collect{|x| prep(x)}
+        else
+          a
+        end
+      end
     end
   end
 end
--- test/wilderpeople/matcher_test.rb
+++ test/wilderpeople/matcher_test.rb
@@ -8,6 +8,7 @@ module Wilderpeople
         input: 'robert',
         resultset: {
           'robert' => true,
+          'rob ert' => false,
           'rob' => false,
           'robertson' => false,
           'foo' => false,
@@ -20,12 +21,25 @@ module Wilderpeople
       )
     end
 
+    def test_exact_is_case_insensitive
+      assert_with_set(
+        method: :exact,
+        input: 'foo',
+        resultset: {
+          'Foo' => true,
+          'FOO' => true,
+          'fOO' => true
+        }
+      )
+    end
+
     def test_transposed
       assert_with_set(
         method: :transposed,
         input: 'robert',
         resultset: {
           'robert' => true,
+          'rob ert' => false,
           'rob' => false,
           'robertson' => false,
           'foo' => false,
@@ -38,12 +52,32 @@ module Wilderpeople
       )
     end
 
+    def test_transposed_with_alternative_case
+      assert_with_set(
+        method: :transposed,
+        input: 'robert',
+        resultset: {
+          'Robert' => true,
+          'Rob ert' => false,
+          'Rob' => false,
+          'Robertson' => false,
+          'Foo' => false,
+          'Nobert' => false,
+          'Trebor' => true,
+          'Retbor' => true,
+          'Robert bruce' => false,
+          '' => false
+        }
+      )
+    end
+
     def test_exact_except_last_word
       assert_with_set(
         method: :exact_except_last_word,
         input: 'robert',
         resultset: {
           'robert' => true,
+          'rob ert' => false,
           'rob' => false,
           'robertson' => false,
           'foo' => false,
@@ -56,11 +90,49 @@ module Wilderpeople
       )
     end
 
+    def test_first_letter
+      assert_with_set(
+        method: :first_letter,
+        input: 'robert',
+        resultset: {
+          'robert' => true,
+          'rob ert' => true,
+          'rob' => true,
+          'robertson' => true,
+          'foo' => false,
+          'nobert' => false,
+          'trebor' => false,
+          'retbor' => true,
+          'robert bruce' => true,
+          '' => false
+        }
+      )
+    end
+
+    def test_exact_no_space
+      assert_with_set(
+        method: :exact_no_space,
+        input: 'robert',
+        resultset: {
+          'robert' => true,
+          'rob ert' => true,
+          'rob' => false,
+          'robertson' => false,
+          'foo' => false,
+          'nobert' => false,
+          'trebor' => false,
+          'retbor' => false,
+          'robert bruce' => false,
+          '' => false
+        }
+      )
+    end
+
     def assert_with_set(method: '', input: '', resultset: {})
       resultset.each do |match, expected|
         assert_equal(
           expected,
-          matcher.send(method, input, match),
+          matcher.send(method, input, match.dup), # match is a hash key so frozen. Using dup to create unfrozen copy
           "'#{input}' should#{' not' unless expected} match '#{match}' with `matcher.#{method}`"
         )
       end
